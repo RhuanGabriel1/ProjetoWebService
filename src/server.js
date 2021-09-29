ac778829766278a26ab70a2181644403d65d26cb
safrapc <leandrosafra.jconectada@gmail.com>
2021-09-28 21:48:25 -0300

    feat/add database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
+const mysql = require("mysql2/promise");
 const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(router);
-app.listen(4000, function () {
-  console.log("Server is running on port 4000");
-});
+(async () => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(router);
+
+  const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "admin",
+    port: "3306",
+    database: "testedb",
+  });
+  console.log("Conectou no MySQL!");
+  app.listen(4000, function () {
+    console.log("Server is running on port 4000");
+  });
+})();
